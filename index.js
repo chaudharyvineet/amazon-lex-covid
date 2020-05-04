d42b2f4769b390c34e26aec01043b410ca3d7567
Vineet Chaudhary <vineetchaudhary131@gmail.com>
2020-05-04 15:19:29 +0530

    updated api lambda function node 12.x

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 'use strict';
+//exports.handler = async (event) => {
 
-exports.handler = async (event) => {
+
+ function close(sessionAttributes, fulfillmentState, message) {
+        return {
+            'sessionAttributes' : sessionAttributes,
+            'dialogAction': {
+                'type': 'Close',
+                'fulfillmentState' : fulfillmentState ,
+                'message': message,
+            },
+        };
+    }
+
+async function dispatch (intentRequest, callback) {
     const https = require('https');
-    const sessionAttributes = event.sessionAttributes;
-    const slots = event.currentIntent.slots;
-    const fever = slots.fever;
-    const status = slots.status;
-    const cough = slots.cough;
+    const sessionAttributes = intentRequest.sessionAttributes;
+    const slots = intentRequest.currentIntent.slots;
+    var data = slots.data;
+ 
+    
     let dataString = '';
     
     const response = await new Promise((resolve, reject) => {
@@ -18,7 +31,7 @@ exports.handler = async (event) => {
             resolve({
                
                 body: JSON.parse(dataString)
-            });
+                });
           });
         });
         
@@ -30,22 +43,50 @@ exports.handler = async (event) => {
         });  
     });
     
-   let sol = response.body.result;
-    //return sol;
+  let sol = JSON.stringify(response.body.result);
+ 
+//   return sol;
+//   let respo = { sessionAttributes: event.sessionAttributes,
+//     "dialogAction": {
+//         "type": "ConfirmIntent",
+//         "fulfillmentState": "Fulfilled",
+//         "message": {
+//             "contentType": "SSML",
+//             "content": sol
+//         }
+//     }
+// };
+//         return respo.dialogAction.message.content;
   
-  let respo = { sessionAttributes: event.sessionAttributes,
-    "dialogAction": {
-        "type": "ConfirmIntent",
-        "fulfillmentState": "Fulfilled",
-        "message": {
-            "contentType": "application/json",
-            "content": sol
+//};
+
+
+
+    // const rense = {
+    //     statusCode: 200,
+    //     body: JSON.stringify('Hello from Lambda!'),
+    // };
+    // return response;
+    
+    callback(close(sessionAttributes, 'Fulfilled',
+        {'contentType': 'PlainText', 'content': 'Here is the current data about COVID-19 cases in India ' + sol + ' This app does not say about your symptoms with any surety. We help you understand the scenario. To be sure, please consult a doctor. Thank you. STAY SAFE, STAY HOME.'}));
+};
+
+  exports.handler = (event, context, callback) => {
+        try {
+            dispatch(event,
+                (response) => {
+                    callback(null, response);
+                });
+        } catch (err) {
+            callback(err);
         }
-    }
-};
-        return respo.dialogAction.message.content;
-  
-};
+    };
+
+
+
+
+ 
 
 
 //https://api.covid19api.com/live/country/south-africa/status/confirmed
